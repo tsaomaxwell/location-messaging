c55314d05ff491cd5803a4602feaea1b9f9b66c9
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-02-08 16:26:18 -0800

    finished adding starter code

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// Import the screens
+import Main from './components/Main';
+import Chat from './components/Chat';
+// Import React Navigation
+import { createStackNavigator } from 'react-navigation'
+// Create the navigator
+const navigator = createStackNavigator({
+  Main: { screen: Main },
+  Chat: { screen: Chat },
 });
+// Export it as the root component
+export default navigator;
